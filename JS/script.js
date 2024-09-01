1fccd12e384fd640fcefbd9a63769e2213b3ce55
ronaldo <naldo_hp@hotmail.com>
2024-09-01 05:34:37 -0300

    teste if

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,14 @@
 const menuHamburguer = document.querySelector('.menu-hamburguer');
 menuHamburguer.addEventListener('click', () => {
-        console.log("ok"); //LOG
+        console.log("funciona meu filho"); //LOG
+            });
 
-    });
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+    if (menuHamburguer.classList.contains('change')) {
+        nav.computedStyleMap.display 'block';
+        {}
+    }
+}
